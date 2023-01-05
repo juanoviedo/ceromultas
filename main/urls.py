56356721bb39cd3a644ffeab3b15083356e6d35b
Juan Oviedo <juan.oviedo.lutkens@gmail.com>
2023-01-04 22:11:21 -0500

    Commit

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    # path('innovaciones/', views.innovaciones, name='innovaciones'),
-    # path('register/', views.register, name='contacto'),
-]
--- main/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import response
-from django.shortcuts import render
-
-def inicio(response):
-    return render(response, 'main/inicio.html', {})
